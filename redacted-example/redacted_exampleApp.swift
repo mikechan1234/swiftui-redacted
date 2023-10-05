6b7ba6ccebac1adb94c60c47f34b5e50b27788f6
Michael Chan <mikechan1234@gmail.com>
2023-10-05 15:28:46 +0900

    Initial Commit

---
+++ redacted-example/redacted_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  redacted_exampleApp.swift
+//  redacted-example
+//
+//  Created by Michael Chan on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct redacted_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
